803339ee6679b483197050ebe3aab3852f4203f5
Otto0811 <enriquezumeta@gmail.com>
2021-08-28 12:50:24 -0600

    Parcial 1

---
+++ ConexionPostgre1/src/main/java/uspg/edu/Conectar.java
@@ -0,0 +1,14 @@
+package uspg.edu;
+
+import com.sun.jdi.connect.spi.Connection;
+
+
+
+public class Conectar {
+
+private static Connection instancia;
+	
+	private Conectar() {}
+	
+	}
+
---
+++ ConexionPostgre1/src/main/java/uspg/edu/ConexionPostgre1Application.java
@@ -0,0 +1,13 @@
+package uspg.edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+public class ConexionPostgre1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConexionPostgre1Application.class, args);
+	}
+
+}
